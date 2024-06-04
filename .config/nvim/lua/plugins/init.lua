7f0d882a259597bf3ba6a6054c94a402c0149f20
Anders Kaae <anders.kaae@gmail.com>
2024-06-04 08:38:28 +0200

    Added TODO hightlighting

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -58,5 +58,14 @@ return {
       -- or run <leader>ch to see copilot mapping section
     end
   },
-
+  {
+    "folke/todo-comments.nvim",
+    lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
 }
