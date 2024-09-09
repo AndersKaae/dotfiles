bbab8c7f331e828e95f442d2feeefc67d9b8f840
Anders Lyager Kaae <anders.kaae@gmail.com>
2024-09-09 21:54:32 +0200

    Fixed the bindings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -5,7 +5,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", "½", "$", { desc = "Go to end of line" })
-
+map({ "n", "v" }, "½", "$", { desc = "Go to end of line" })
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map('i', '<C-L>', 'copilot#Accept("\\<CR>")', {expr = true, replace_keycodes = false })
