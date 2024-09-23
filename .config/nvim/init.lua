c89906cdf011bece669bef6545a86226c59b2e56
Anders Lyager Kaae <anders.kaae@gmail.com>
2024-09-23 21:15:02 +0200

    Added go fmt on save

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -37,3 +37,8 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function() vim.lsp.buf.format({ async = false }) end,
+})
