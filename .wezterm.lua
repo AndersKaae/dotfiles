d0693ce1ccc1f4af4c3f3abe90dd24ddc50cff6e
Anders Kaae <anders.kaae@gmail.com>
2024-11-01 12:56:20 +0100

    made brackets work

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,4 +1,11 @@
 return {
+    -- Set the color scheme to Dracula
     color_scheme = "Dracula (Official)",
+
+    -- Disable the fancy tab bar
     use_fancy_tab_bar = false,
+
+    -- Make brackets work with Danish keyboard on macOS
+    send_composed_key_when_left_alt_is_pressed = true,
+    send_composed_key_when_right_alt_is_pressed = true,
 }
