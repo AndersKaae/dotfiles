74d74b1601431ed4db792f9ff1c1ac8f4cfd4293
Anders Lyager Kaae <anders.kaae@gmail.com>
2025-03-31 20:55:41 +0200

    added autopilot

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -226,6 +226,18 @@ require("lazy").setup({
 	},
 
 	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = { enabled = true },
+				panel = { enabled = true },
+			})
+		end,
+	},
+
+	{
 		"goolord/alpha-nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
