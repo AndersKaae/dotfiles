34723f917c06b3b4613e661896eec26af2ca37bf
Anders Lyager Kaae <anders.kaae@gmail.com>
2025-05-21 20:43:04 +0200

    added files for arch and hyprland

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -739,25 +739,18 @@ require("lazy").setup({
 				gopls = {},
 				pyright = {},
 				-- rust_analyzer = {},
-				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				--
-				-- Some languages (like typescript) have entire language plugins that can be useful:
-				--    https://github.com/pmizio/typescript-tools.nvim
-				--
-				-- But for many setups, the LSP (`ts_ls`) will work just fine
-				-- ts_ls = {},
-				--
+				-- tsserver = {}, -- optional, for JS/TS files outside Vue
+
+				["vue-language-server"] = {
+					filetypes = { "vue", "javascript", "typescript" }, -- Add JS/TS if needed
+				},
 
 				lua_ls = {
-					-- cmd = { ... },
-					-- filetypes = { ... },
-					-- capabilities = {},
 					settings = {
 						Lua = {
 							completion = {
 								callSnippet = "Replace",
 							},
-							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
 							-- diagnostics = { disable = { 'missing-fields' } },
 						},
 					},
@@ -1046,6 +1039,8 @@ require("lazy").setup({
 				"vimdoc",
 				"go",
 				"python",
+				"vue",
+				"typescript",
 			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
