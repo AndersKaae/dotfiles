df5887419d013173b4bcb10e4aaa1eb424b11e41
Anders Lyager Kaae <anders.kaae@gmail.com>
2025-06-15 12:08:18 +0200

    Added commentary plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -170,6 +170,7 @@ require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 	"jlcrochet/vim-razor",
+	"tpope/vim-commentary", -- Commenting plugin
 
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
